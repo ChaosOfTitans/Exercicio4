bcf9e130dfd8d5050cd5f0f7c30fd027d112b616
Ubuntu <ubuntu@ip-172-31-20-65.ec2.internal>
2020-10-26 17:17:45 +0000

    primeiro

---
+++ matricula.py
@@ -0,0 +1,22 @@
+import rospy
+from std_msgs.msg import String
+
+
+rospy.init_node('Atividade_4')
+
+num = 'matricula = 2017001704'
+
+def num_callBack(msg):
+    global num
+    num = msg.data
+
+def timerCallBack(event):
+    msg = String()
+    msg.data = '2017001704000000000'
+    pub.publish(msg)
+    
+pub = rospy.Publisher('/matricula', String, queue_size = 1)
+timer = rospy.Timer(rospy.Duration(0.05), timerCallBack)
+sub = rospy.Subscriber('/soma', String, num_callBack)
+
+rospy.spin()

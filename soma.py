157fb3ccfc88cf2b75302df8260ac8989a58e8f7
Ubuntu <ubuntu@ip-172-31-20-65.ec2.internal>
2020-10-26 17:18:28 +0000

    segundo

---
+++ soma.py
@@ -0,0 +1,31 @@
+import rospy
+from std_msgs.msg import String
+
+
+rospy.init_node('Atividade_4_soma')
+
+valor = '10'
+
+def topic_callBack (soma_total):
+    global valor
+    valor = soma_total.data
+    
+    
+    
+rospy.Subscriber('/matricula', String, topic_callBack)
+    
+def timerCallBack(event):
+    soma_total = 0
+    
+    for i in valor:
+        soma_total = soma_total +int(i)
+    msg = String()
+    msg.data = str(soma_total)
+        
+    pub.publish(msg)
+        
+pub = rospy.Publisher('/soma', String, queue_size = 1)
+timer = rospy.Timer(rospy.Duration(0.05), timerCallBack)
+        
+rospy.spin()
+    
